05e55c149488694ebac124fe16ef1898eccbe8ab
Feiko Joosten <feiko_joosten@hotmail.com>
2020-04-21 21:30:19 +0200

    Changed the notification count to use indirect notifications
    Changed the notification count to use indirect notifications instead of direct notifications

--- uncompressed/github/webview.js
+++ uncompressed/github/webview.js
@@ -5,6 +5,6 @@ module.exports = Franz => {
     Franz.setBadge(document.querySelector('[class*="mail-status unread"]') !== null ? 1 : 0);
   };
 
-  Franz.loop(getMessages);
+  Franz.loop(0, getMessages);
 }
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnaXRodWIvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
