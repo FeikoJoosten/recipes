82cb65a9fa605f56b8eca33101f7c831b39e161b
Feiko Joosten <feiko_joosten@hotmail.com>
2020-04-22 08:53:40 +0200

    Fixed an incorrect loop
    Fixed an issue where I accidentally added 0, to the loop function instead of the setBadge function.

--- uncompressed/github/webview.js
+++ uncompressed/github/webview.js
@@ -2,9 +2,9 @@
 
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    Franz.setBadge(document.querySelector('[class*="mail-status unread"]') !== null ? 1 : 0);
+    Franz.setBadge(0, document.querySelector('[class*="mail-status unread"]') !== null ? 1 : 0);
   };
 
-  Franz.loop(0, getMessages);
+  Franz.loop(getMessages);
 }
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnaXRodWIvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
